c4bfb5f5a645152f9a5652c6833b9341ce6f8be9
lyne007 <lyne007@163.com>
2021-10-08 05:40:51 +0000

    安装mongodb包

--- mongodb-demo/config/app.php
+++ mongodb-demo/config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
         /*
          * Application Service Providers...
