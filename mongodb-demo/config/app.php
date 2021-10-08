fdb02fca9db4396212a93d62ada9d4e9c517d6fc
lyne007 <lyne007@163.com>
2021-10-08 06:53:13 +0000

    初始化mongodb-demo

--- mongodb-demo/config/app.php
+++ mongodb-demo/config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
         /*
          * Application Service Providers...
